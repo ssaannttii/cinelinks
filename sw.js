5aa250e39ecb41a73564a2b7e1e56d8584cef3e2
Santi <santirv@gmail.com>
2026-06-29 16:26:42 +0200

    CineGroup anti-repetition: +8 sagas (Fast&Furious, Bourne, X-Men, Transformers, Twilight, Alien, Terminator, MiB) +8 themes (submarine, prison, mob, alien invasion, AI, WWII, survival, serial killers), TMDB IDs verified. SW v60

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *   - other same-origin GETs (logo, icons, daily-challenges.js, i18n.js): stale-while-revalidate.
  * Bump CACHE_VERSION to invalidate old caches on deploy.
  */
-const CACHE_VERSION = 'cinelinks-v59';
+const CACHE_VERSION = 'cinelinks-v60';
 const SHELL = [
   '/',
   '/index.html',
